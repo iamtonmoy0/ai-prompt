2f9fe0950c003dcabdb60eed929a627d7a50eb47
iamtonmoy0 <tonmoy2code@gmail.com>
2024-01-07 22:34:11 +0600

    auth provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Provider from "@/components/Provider";
 
 const inter = Roboto({ subsets: ["latin"], weight: ["100", "300"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <Provider>
+          <Navbar />
+          {children}
+        </Provider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
   const isUserLoggedIn = true;
+
+  const [toggleDropdown, setToggleDropdown] = useState(false);
   return (
-    <div className="w-full  flex justify-between mb-16 pt-3">
+    <div className="w-full  flex justify-between mb-5 pt-3">
       <Link href="/" className="flex">
         <Image
           src="/assets/images/logo.svg"
@@ -18,7 +20,7 @@ export default function Navbar() {
       </Link>
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
-          <div className="flex gap-3 ">
+          <div className="flex gap-3 mr-5 ">
             <Link
               href="/create-prompt"
               className=" border border-spacing-1 rounded-full p-2 hover:bg-black hover:text-white"
@@ -26,19 +28,24 @@ export default function Navbar() {
               Create Post
             </Link>
             <button className=" border border-spacing-1 rounded-full p-2 hover:bg-black hover:text-white">
-              Signout
+              Sign Out
             </button>
             <Link href="/profile">
               <Image
                 src="/assets/images/logo.svg"
                 alt="logo"
+                className="rounded-full pt-1"
                 width={30}
                 height={30}
               />
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            <button className=" border border-spacing-1 rounded-full p-2 hover:bg-black hover:text-white mr-6">
+              Sign In
+            </button>
+          </>
         )}
       </div>
     </div>
